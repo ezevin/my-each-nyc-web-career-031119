a3ddfd47a4a3acb0062b66f45f67c6e3afd1c420
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-13 17:35:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,11 @@
 def my_each(array)   # put argument(s) here
   # code here
+  if block_given?
   i = 0 
   while array.length > i
   array.each do |word|
     puts word
-    i += 1
+    i = i + 1
   end 
 end 
 end
