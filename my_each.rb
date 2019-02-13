7a8206a1cd874e881f68959ed69f511ee532a763
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-13 17:33:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,9 @@
-def my_each # put argument(s) here
+def my_each(array)   # put argument(s) here
   # code here
-  
+  i = 0 
+  while array.length > i
+  array.each do |word|
+    puts word
+    i += 1
+  end 
 end
